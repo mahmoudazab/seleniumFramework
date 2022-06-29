67dead341c60a9a16d4dadf7db6fc54c4d3bf9ad
mahmoudazab <mahmoudazab97@yahoo.com>
2022-06-29 16:17:27 +0200

    Add headless testing with  phantom

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -6,6 +6,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriverService;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Optional;
@@ -33,6 +36,18 @@ public class TestBase extends AbstractTestNGCucumberTests{
 			System.setProperty("webdriver.gecko.driver" , System.getProperty("user.dir") + "\\drivers\\geckodriver.exe");
 			driver = new FirefoxDriver ();
 			
+		}
+		//Headless browser testing
+		else if (browserName.equalsIgnoreCase("headless"))
+		{
+			DesiredCapabilities caps = new DesiredCapabilities();
+			caps.setJavascriptEnabled(true);
+			caps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY
+					,System.setProperty("phantomjs.binary.path"
+							, System.getProperty("user.dir") + "\\drivers\\phantomjs.exe")) ;
+			String [] phantomJsArgs = {"--web-security=no" , "--ignore-ssl-errors=yes"};
+			caps.setCapability(PhantomJSDriverService.PHANTOMJS_CLI_ARGS,phantomJsArgs);
+ 			driver = new PhantomJSDriver();
 		}
 		else if (browserName.equalsIgnoreCase("ie"))
 		{
